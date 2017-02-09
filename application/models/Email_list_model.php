33772fd5d7bd4c6948ff9b804dd55ab2b54bdaad
Léo Castro <leonardo_carvalho@outlook.com>
2017-02-09 13:35:51 -0200

    Creating email list action

---
+++ application/models/Email_list_model.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Email_list_model extends CI_Model
+{
+    /**
+     *
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+
+
+}
