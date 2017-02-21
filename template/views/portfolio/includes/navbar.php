97ef0643aff1558eac0423c3a774c7bc716acf9d
Léo Castro <leonardo_carvalho@outlook.com>
2017-02-21 17:32:00 -0300

    Added 'nofollow' in button back

--- template/views/portfolio/includes/navbar.php
+++ template/views/portfolio/includes/navbar.php
@@ -7,7 +7,7 @@
                 <img title="Logo" src="<?php echo asset('img/logo/logo-small.png') ?>" alt="Logo do website"/>
             </a>
         </div>
-        <a title="Voltar para o portfólio" href="<?php echo route('/#portfolio') ?>" class="btn btn-dark navbar-btn" role="button">
+        <a title="Voltar para o portfólio" href="<?php echo route('/#portfolio') ?>" class="btn btn-dark navbar-btn" role="button" rel="nofollow">
             <i class="material-icons" style="vertical-align: -29%">replay</i>
             Voltar
         </a>
