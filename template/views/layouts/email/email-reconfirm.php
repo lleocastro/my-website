3fa0d5aa7e200a162e3f386a3d84edcfa5a78eb8
Léo Castro <leonardo_carvalho@outlook.com>
2017-02-20 21:37:19 -0300

    Reconfirm email template created

---
+++ template/views/layouts/email/email-reconfirm.php
@@ -0,0 +1,22 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+Opa! Tudo bem?
+
+A alguns dias mandei um email para confirmar o seu cadastro na minha lista de emails
+para receber um orçamento (preview), mas você não confirmou...
+
+Então pensei que talvez você não tenha recebido o email ou talvez tenha ido para a caixa
+de spam ou lixeira. Ai resolvi mandar de novo. ;)
+
+Para confirmar seu email, clique neste link: <a href="<?php echo $link ?>"><?php echo $link ?></a>
+
+
+PS: Caso seja um engano apenas desconsidere.
+
+
+Caso queira sair da minha lista clique neste link:
+
+...
+Léo B. Castro
+(<a href="<?php echo base_url() ?>"><?php echo base_url() ?></a>)
+(<a href="https://www.linkedin.com/in/leobcastro/">https://www.linkedin.com/in/leobcastro/</a>)
+CEO
---
+++ template/views/layouts/email/reconfirm-budget.php
@@ -0,0 +1,17 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+Opa! Tudo bem?
+
+A alguns dias mandei um email para um orçamento de uma solução web para seu negócio/empresa
+e você não me respondeu... Então talvez você não tenha recebido ou
+talvez tenha apagado por engano.
+
+Caso ainda tenha interesse responda esse email o requisitando novamente. :)
+
+
+Caso queira sair da minha lista clique neste link:
+
+...
+Léo B. Castro
+(<a href="<?php echo base_url() ?>"><?php echo base_url() ?></a>)
+(<a href="https://www.linkedin.com/in/leobcastro/">https://www.linkedin.com/in/leobcastro/</a>)
+CEO
