70a37eb54bc00e8ea7c86ebd611465017d4115c7
Léo Castro <leonardo_carvalho@outlook.com>
2017-02-21 22:37:22 -0300

    Updated text structure of email template 'reconfirm-email'

--- template/views/layouts/email/reconfirm-email.php
+++ template/views/layouts/email/reconfirm-email.php
@@ -1,19 +1,31 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 Opa! Tudo bem?
 
-A alguns dias mandei um email para confirmar o seu cadastro na minha lista de emails
-para receber um orçamento (preview), mas você não confirmou...
+A alguns dias mandei um email para confirmar o seu cadastro na minha lista de emails,
+mas você não confirmou... É para conversarmos um pouco sobre uma solução web que iria resolver
+problemas na sua empresa/negócio.
 
-Então pensei que talvez você não tenha recebido o email ou talvez tenha ido para a caixa
-de spam ou lixeira. Ai resolvi mandar de novo. ;)
+<strong>Sem compromisso! Apenas uma conversa...</strong>
+
+Podemos falar sobre:
+
+- Um sistema para automatizar processos e consequentemente diminuir custos.
+- Conquistar novos clientes e manter contato com os atuais através de um blog, portal,
+site, email marketing ou newletter.
+- Planejamento web completo.
+- Etc..
+
+"Investir em novos meios para se obter mais resultados é sempre bom, e a web é o lugar certo para isso. :)
+
 
 Para confirmar seu email, clique neste link: <a href="<?php echo $link ?>"><?php echo $link ?></a>
 
 
 PS: Caso seja um engano apenas desconsidere.
+Caso queira sair da minha lista clique neste link: xxx
 
 
-Caso queira sair da minha lista clique neste link:
+Obrigado.
 
 ...
 Léo B. Castro
