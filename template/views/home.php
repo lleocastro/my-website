60681ce527f6e7c3b30bbf519e760a99250d286c
Léo Castro <leonardo_carvalho@outlook.com>
2017-03-16 19:30:25 -0300

    Added share buttons to phrase

--- template/views/home.php
+++ template/views/home.php
@@ -272,6 +272,44 @@
                             </div>
                         </div>
                     </div>
+                    <hr>
+                    <hr>
+                    <p>
+                        <em><strong>"</strong>Produtividade não é o quanto você trabalha e sim como você trabalha e a qualidade do que entrega.<strong>"</strong></em>
+                        <div id="fb-root"></div>
+                        <script>(function(d, s, id) {
+                                var js, fjs = d.getElementsByTagName(s)[0];
+                                if (d.getElementById(id)) return;
+                                js = d.createElement(s); js.id = id;
+                                js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.8";
+                                fjs.parentNode.insertBefore(js, fjs);
+                            }(document, 'script', 'facebook-jssdk'));</script>
+                        <div class="fb-share-button" data-href="http://www.leobcastro.com.br/" data-layout="button_count" data-size="small" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fwww.leobcastro.com.br%2F&amp;src=sdkpreparse">Compartilhar</a></div>
+                        &nbsp;
+                        <script>window.twttr = (function(d, s, id) {
+                                var js, fjs = d.getElementsByTagName(s)[0],
+                                    t = window.twttr || {};
+                                if (d.getElementById(id)) return t;
+                                js = d.createElement(s);
+                                js.id = id;
+                                js.src = "https://platform.twitter.com/widgets.js";
+                                fjs.parentNode.insertBefore(js, fjs);
+
+                                t._e = [];
+                                t.ready = function(f) {
+                                    t._e.push(f);
+                                };
+
+                                return t;
+                            }(document, "script", "twitter-wjs"));</script>
+                        <a class="twitter-share-button"
+                           href="https://twitter.com/intent/tweet?text='Produtividade não é o quanto você trabalha e sim como você trabalha e a qualidade do que entrega.'">
+                            Tweet
+                        </a>
+                        &nbsp;
+                        <a href="https://twitter.com/leobcastro94" class="twitter-follow-button" data-show-count="false">Follow @leobcastro94</a><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+                    </p>
+                    <hr>
                 </div>
             </div>
         </section><!-- END SKILLS -->
